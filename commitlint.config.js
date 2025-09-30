559de50f61508d46938d157ebfe23c0c3415dbfd
Marcelo <mcomanduci@gmail.com>
2025-09-30 10:03:19 -0300

    feat: add commitlint for commit message validation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',     // New feature
-        'fix',      // Bug fix
-        'docs',     // Documentation changes
-        'style',    // Code style changes (formatting, etc.)
-        'refactor', // Code refactoring
-        'perf',     // Performance improvements
-        'test',     // Adding or updating tests
-        'chore',    // Maintenance tasks
-        'ci',       // CI/CD changes
-        'build',    // Build system changes
-        'revert'    // Revert a previous commit
-      ]
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Code style changes (formatting, etc.)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Adding or updating tests
+        "chore", // Maintenance tasks
+        "ci", // CI/CD changes
+        "build", // Build system changes
+        "revert", // Revert a previous commit
+      ],
     ],
-    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
-  }
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+  },
 };
